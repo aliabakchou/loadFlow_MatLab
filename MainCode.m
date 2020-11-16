f31e3cef176cda9822b8bff32a63893eb672d61b
aliabakchou <66357009+aliabakchou@users.noreply.github.com>
2020-11-16 21:42:14 +0100

    Update MainCode.m

--- MainCode.m
+++ MainCode.m
@@ -13,5 +13,22 @@ busd = busdatas( );      % les donnees de bus 14
 disp('les resultats obtener par la methode NR ');
 
 [voltage , theta] =newton_raphson(Y,busd)
+[Pi Qi Pg Qg Pl Ql]=loadflow(voltage,theta);
+disp('Voltage:');
 disp(voltage);
+disp('angle: ');
 disp(theta);
+disp('elem active power');
+
+disp(Pi);
+disp('elem reactive power ');
+
+disp(Qi);
+disp('bus generator active power ');
+disp(Pg);
+disp('bus generator reactive power ');
+disp(Qg);
+disp('load active power ');
+disp(Pl);
+disp('load reactive power');
+disp(Ql);
